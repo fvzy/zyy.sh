989a5f074f5b19a4e0f7b12ba3ef88216c9ec9c1
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-06-27 18:25:04 -0400

    Landing text changes

--- frames/Landing.tsx
+++ frames/Landing.tsx
@@ -1,5 +1,3 @@
-import DefText from "../components/DefText";
-
 const Landing = () => {
     return (
     <div className="flex flex-col justify-center items-start h-[95vh] pb-[10vh] space-y-2">
@@ -7,10 +5,10 @@ const Landing = () => {
             <h1>Hello!</h1>
             <h1>I'm Kevin.</h1>
         </div>
-        <DefText>
+        <p className="inter-medium text-sub-light">
             I'm a tri-brid web developer, software engineer, and UI designer.
             I build stunning websites and practical applications using the latest technologies.
-        </DefText>
+        </p>
     </div>
     )
 }
