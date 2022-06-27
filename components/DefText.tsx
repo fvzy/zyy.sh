637fd1230e1a3bfd78284d0987dedbefe2e2773f
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-06-27 18:23:30 -0400

    DefText uses text-ms

--- components/DefText.tsx
+++ components/DefText.tsx
@@ -1,8 +1,8 @@
 // TODO: Add dark theme text color variant
-const DefText = ({ className, children }: { className?: string, children: string }) => {
-    return (<p className={"inter-medium text-sub-light " + className}>
+const DefText = ({ className = "", children }: { className?: string, children: string }) => {
+    return (<p className={"inter-medium text-sub-light text-ms " + className}>
         { children }
     </p>)
 }
 
-export default DefText;
+export default DefText; 
