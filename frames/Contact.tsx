72ddf4191c9d37781d0df5e068191e036c21edef
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-06-30 19:34:10 -0400

    Contact.tsx

---
+++ frames/Contact.tsx
@@ -0,0 +1,26 @@
+import Button from "../components/Button";
+import DefText from "../components/DefText";
+import HeadingTwo from "../components/HeadingTwo";
+import Preface from "../components/Preface";
+
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+
+const Contact = () => {
+	return (
+		<div className="space-y-1">
+			<Preface className="text-blue-light text-center">CONTACT</Preface>
+			<HeadingTwo className="text-center">Let's Start Working!</HeadingTwo>
+			<DefText className="text-center">
+				Whether you're a startup searching for another passionate
+				developer or an established organization looking to get things
+				done, I'm here to make your vision a reality. Click the button
+				below to send me an email, and let's start working!
+			</DefText>
+            <div className="flex justify-center pt-4">
+                <Button icon={faEnvelope} text="Email Me!" src="mailto:hello@liao.gg" />
+            </div>
+		</div>
+	);
+};
+
+export default Contact;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import Landing from "../frames/Landing";
+import Medium from "../frames/Medium";
+import Projects from "../frames/Projects";
 import Skills from "../frames/Skills";
+import Contact from "../frames/Contact";
 
 const Home = () => {
     return (
         <div>
             <Landing />
-            <Skills />
+            <div className="space-y-32">
+                <Skills />
+                <Projects />
+                <Medium />
+                <Contact />
+            </div>
         </div>
     )
 }
