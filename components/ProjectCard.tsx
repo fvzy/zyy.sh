14208c7900ecda854ffd9413987dd5fd61943a9e
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-07-03 11:11:41 -0400

    Project card styling changes

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -9,7 +9,7 @@ const ProjectCard =
             <div className="absolute bg-transparent dark:bg-off-black !bg-opacity-25 w-full h-full z-10" />
             <Image src={image} alt={imageAlt} className="rounded-t-md" layout="fill" objectFit="cover" priority={false} />
         </div>
-        <div className="border-[#d0d7de] dark:border-[#333333] dark:bg-[#151515] border-x border-b px-4 pt-3 pb-4 rounded-b-lg space-y-1">
+        <div className="border-[#d0d7de] dark:border-[#333333] bg-off-white bg-opacity-25 dark:bg-[#1A1A1A] border-x border-b px-4 pt-3 pb-4 rounded-b-lg space-y-1">
             <h3 className="mono-bold text-main-light dark:text-main-dark text-lg">{ title }</h3>
             <p className="mono-regular text-sub-light dark:text-sub-dark text-xs">{ text }</p>
         </div>
