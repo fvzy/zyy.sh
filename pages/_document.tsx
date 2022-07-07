a24e6cf7783f149a82b93afed306d00ff4b51998
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-07-07 16:46:37 -0400

    html specify language

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class Portfolio extends Document {
     render() {
         return (
-            <Html>
+            <Html lang="en">
                 <Head>
                     <script dangerouslySetInnerHTML={{__html: `
         (function() {
