6fc6f7453ba13d0e91deb50e99fa0e10bd8bdf6b
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-07-12 20:16:17 -0400

    Custom classnames for FrameWrapper

--- components/FrameWrapper.tsx
+++ components/FrameWrapper.tsx
@@ -1,5 +1,5 @@
-const FrameWrapper = ({ children }) => {
-    return <div className="space-y-1 px-5">
+const FrameWrapper = ({ children, className = "" }) => {
+    return <div className={"mx-auto space-y-1 px-5 " + className}>
         { children }
     </div>
 }
