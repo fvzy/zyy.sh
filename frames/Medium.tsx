9bbfa6840595e7d6f9a3308882d494f8700b7157
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-07-21 21:06:27 -0400

    Medium md lg styling

--- frames/Medium.tsx
+++ frames/Medium.tsx
@@ -4,30 +4,43 @@ import Preface from "../components/Preface";
 import Button from "../components/Button";
 import Image from "next/image";
 
-import phoneLight from "../images/phone-light.jpg";
-import phoneDark from "../images/phone-dark.jpg";
+import phoneLight from "../images/phone-light.png";
+import phoneDark from "../images/phone-dark.png";
 import { faMedium } from "@fortawesome/free-brands-svg-icons";
 import FrameWrapper from "../components/FrameWrapper";
 
 const Medium = () => {
-	return <FrameWrapper className="max-w-[800px] md:flex md:space-y-0 md:space-x-12">
+	return <FrameWrapper className="max-w-[800px] sm:flex sm:space-y-0 sm:space-x-6 md:space-x-12">
+        <div className="space-y-1">
             <Preface className="text-purple-light dark:text-purple-dark">MEDIUM</Preface>
             <HeadingTwo>Check Out My Writing Too!</HeadingTwo>
-        <DefText>
-            Check out my blog on Medium, where I write about various topics
-            from productivity to computer science and technology on a weekly,
-            biweekly basis.
-        </DefText>
-        <DefText>
-            Whether you're a web developer, data scientist, tech enthusiast,
-            or anyone really, there's a read for you. 100% free.
-        </DefText>
-        <div className="relative w-full h-[450px] -z-10 !-mt-3">
-            <Image src={phoneLight} className="dark:invisible" alt="iPhone with Medium article on screen" layout="fill" objectFit="cover" />
-            <Image src={phoneDark} className="invisible dark:visible" alt="iPhone with Medium article on screen" layout="fill" objectFit="cover" />
+            <div className="space-y-1 lg:space-y-2">
+                <DefText>
+                    Check out my blog on Medium, where I write about various topics
+                    from productivity to computer science and technology on a weekly,
+                    biweekly basis.
+                </DefText>
+                <DefText>
+                    Whether you're a web developer, data scientist, tech enthusiast,
+                    or anyone really, there's a read for you. 100% free.
+                </DefText>
+                <div className="justify-center hidden sm:flex pt-3">
+                    <Button icon={faMedium} text="Medium" src="https://liaogg.medium.com" />
+                </div>
+            </div>
         </div>
-        <div className="flex justify-center">
-            <Button icon={faMedium} text="Medium" src="https://liaogg.medium.com" />
+        <div className="basis-2/5 flex-shrink-0 sm:-translate-y-[1.5rem] md:-translate-y-[3rem]">
+            <div className="p-4 flex justify-center">
+                <div className="dark:hidden">
+                    <Image src={phoneLight} alt="iPhone with Medium article on screen" objectFit="contain" />
+                </div>
+                <div className="hidden dark:block">
+                    <Image src={phoneDark} alt="iPhone with Medium article on screen" objectFit="contain" />
+                </div>
+            </div>
+            <div className="flex justify-center sm:hidden">
+                <Button icon={faMedium} text="Medium" src="https://liaogg.medium.com" />
+            </div>
         </div>
     </FrameWrapper>
 };
