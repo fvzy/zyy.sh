029d845c9245cdd2c10526382d9cb01b0d8f9ebc
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-07-23 21:04:33 -0400

    Heading Preface spacing

--- components/Preface.tsx
+++ components/Preface.tsx
@@ -1,6 +1,6 @@
 const Preface = ({ className, children }: { className: string, children: string }) => {
     return (
-    <h1 className={"mono-bold text-ms pl-0.5 " + className}>
+    <h1 className={"mono-bold text-ms pl-0.5 lg:text-base leading-none " + className}>
         { children }
     </h1>)
 }
