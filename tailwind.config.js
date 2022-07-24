a6d245b93367925e0d8e7758d729bd1a0d1eb990
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-07-23 21:03:53 -0400

    Slightly lighter text

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
             "off-white": "#EDEDF0",
             "off-black": "#242424",
             "main": {
-                light: "#2B2F33",
+                light: "#363B40",
                 dark: "#858A8F"
             },
             "sub": {
