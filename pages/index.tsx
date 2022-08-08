381a80baeca62147f45d8af6b1f13caf6996717e
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-07 23:57:51 -0400

    Title, meta, spacing changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,18 @@ import Projects from "../frames/Projects";
 import Skills from "../frames/Skills";
 import Contact from "../frames/Contact";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 const Home = () => {
     return (
+        <>
+        <Head>
+            <title>kevinMEH | Portfolio</title>
+            <meta name="description" content="The official portfolio of web developer and software engineer kevinMEH (Kevin Liao)" />
+        </Head>
         <div>
             <Landing />
-            <div className="space-y-32">
+            <div className="space-y-32 md:space-y-48">
                 <Skills />
                 <Projects />
                 <Medium />
@@ -17,6 +23,7 @@ const Home = () => {
                 <Footer />
             </div>
         </div>
+        </>
     )
 }
 
