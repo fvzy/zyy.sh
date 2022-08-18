239e62763b2ba38fddc2ac56b1e5f0c435a9a330
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-18 16:29:58 -0400

    TabDisplay and Slide rewrite

--- components/TabDisplay/Slide.tsx
+++ components/TabDisplay/Slide.tsx
@@ -1,33 +1,30 @@
-import DefText from "../DefText";
+import React from 'react';
 import { SkillGroup } from "./TabDisplay";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface SlideProps {
-    title: string,
-    body: string[],
     skillGroups: SkillGroup[],
     active: boolean,
     backdrop?: boolean
 }
 
 // TODO: Rewrite
-const Slide = ({ title, body, skillGroups, active, backdrop = false }: SlideProps) => {
-    return(<div className={"space-y-2 transition-opacity ease-in-out w-full " + (active ? "z-10 opacity-100 " : "opacity-0 ") + (backdrop ? "invisible" : "absolute")}>
-        <h3 className="text-2xl md:text-3xl mono-bold text-main-light dark:text-main-dark tracking-tight">{title}</h3>
-        {body.map((text, i) => 
-            <DefText key={i}>{text}</DefText>)
-        }
-        <div className="flex overflow-scroll no-scrollbar -mx-5 px-5">
-            {skillGroups.map(({title, skills}, i) => (
-                <div className="min-w-[280px] w-full max-w-[340px] inline-block" key={i}>
-                    <h4 className="mono-medium text-sub-light dark:text-sub-dark pb-1">{ title }</h4>
-                    <ul className="mono-medium text-sub-light dark:text-sub-dark pl-6 list-dash">
-                        {skills.map((skill, j) => 
-                            <li key={j}>{ skill }</li>)
-                        }
-                    </ul>
-                </div>
-            ))}
-        </div>
+const Slide = ({ skillGroups, active, backdrop = false }: SlideProps) => {
+    return(
+    <div className={"flex overflow-scroll no-scrollbar -mx-5 px-5 space-y-2 transition-opacity ease-in-out w-full " + (active ? "z-10 opacity-100 " : "opacity-0 ") + (backdrop ? "invisible" : "absolute")}>
+        {skillGroups.map(({title, skills}, i) => (
+            <div className="min-w-[280px] w-full max-w-[340px] inline-block" key={i}>
+                <h4 className="font-mono font-medium text-sub-light dark:text-sub-dark pb-1">{ title }</h4>
+                <ul className="font-mono font-medium text-sub-light dark:text-sub-dark pl-6">
+                    {skills.map(([icon, text], j) => 
+                        <li key={j}>
+                            <FontAwesomeIcon icon={icon} fixedWidth={true} />
+                            <p className="inline">{text}</p>
+                        </li>)
+                    }
+                </ul>
+            </div>
+        ))}
     </div>)
 }
 
--- components/TabDisplay/TabDisplay.tsx
+++ components/TabDisplay/TabDisplay.tsx
@@ -2,37 +2,36 @@ import React, { useState } from "react";
 import Slide from "./Slide";
 import Tab from "./Tab";
 
-export interface SlideBody {
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+
+export interface Section {
     tabTitle: string,
-    title: string,
-    body: string[],
     skillGroups: SkillGroup[]
 }
 
 export interface SkillGroup {
     title: string,
-    skills: string[]
+    skills: [IconDefinition, string][]
 }
 
 // TODO: Accessibility: Left Right Arrow Keys, 
 // TODO: Rewrite styling.
 // TODO: Add animations.
-const TabDisplay = ({ slideBodies }: { slideBodies: SlideBody[] }) => {
+const TabDisplay = ({ sections }: { sections: Section[] }) => {
     const [active, setActive] = useState(0);
     
     const tabs: JSX.Element[] = [];
     const slides: JSX.Element[] = [];
     let hiddenSlide: JSX.Element;
-    for(let [i, {tabTitle, title, body, skillGroups}] of slideBodies.entries()) {
+    for(let [i, {tabTitle, skillGroups}] of sections.entries()) {
         tabs.push(<Tab active={ i === active } setActiveFunction={i === active ? () => {} : () => setActive(i)} key={i}>
             {tabTitle}
         </Tab>);
         slides.push(
-            <Slide title={title} body={body} key={i}
-            skillGroups={skillGroups} active={ i === active } />
+            <Slide key={i} skillGroups={skillGroups} active={ i === active } />
         );
         if(i === active) {
-            hiddenSlide = <Slide title={title} body={body} skillGroups={skillGroups} active={true} backdrop={true} />
+            hiddenSlide = <Slide skillGroups={skillGroups} active={true} backdrop={true} />
         }
     }
 
@@ -40,10 +39,6 @@ const TabDisplay = ({ slideBodies }: { slideBodies: SlideBody[] }) => {
     <div className="space-y-4 py-2">
         <div className="flex overflow-scroll -mx-5 px-5 no-scrollbar md:mx-0 md:px-0">
             { tabs }
-            <div className="inline-block h-10 w-full border-b-4 
-            [border-image:linear-gradient(to_right,rgb(237,237,240),rgba(237,237,240,0))]
-            dark:[border-image:linear-gradient(to_right,rgb(36,36,36),rgba(36,36,36,0))]
-            ![border-image-slice:1]" />
         </div>
         <div className="relative">
             { slides }
