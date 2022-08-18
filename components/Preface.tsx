7ec7e63621e3bcd95200e58151f2b83fc99720ec
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-18 00:32:33 -0400

    File cleanup for v2

--- components/Preface.tsx
+++
@@ -1,8 +0,0 @@
-const Preface = ({ className, children }: { className: string, children: string }) => {
-    return (
-    <h1 className={"mono-bold text-ms pl-0.5 lg:text-base leading-none " + className}>
-        { children }
-    </h1>)
-}
-
-export default Preface;
