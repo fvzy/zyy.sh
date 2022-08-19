5837c61aec2026d7e8204be217b727bd04dc2b82
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-19 02:21:59 -0400

    _app cleanup and TS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,18 +8,11 @@ import "@fontsource/ibm-plex-mono/500.css";
 import "@fontsource/ibm-plex-mono/600.css";
 import "@fontsource/ibm-plex-mono/700.css";
 
-import '../styles/globals.css'
-
-import NavBar from "../components/NavBar.tsx"
+import "../styles/globals.css";
 
 // TODO: Rewrite
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className="py-5">
-        <NavBar />
-        <Component {...pageProps} />
-    </div>
-  )
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
