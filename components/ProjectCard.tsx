68675e47467f4d873a140ca8f2ac17331c38aa21
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-19 15:55:16 -0400

    ProjectCard Rewrite

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,22 +1,18 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
-// TODO: Rewrite
 const ProjectCard = 
 ({ image, imageAlt, title, text, href }: 
-{ image: any, imageAlt: string, title: string, text: string, href: string }) => {
+{ image: string | StaticImageData | { default: StaticImageData }, imageAlt: string, title: string, text: string, href: string }) => {
     return(
-    <a className="flex flex-col md:flex-row-reverse cursor-pointer md:h-44" href={href}>
-        <div className="relative h-[160px] w-auto flex-none md:basis-1/3 md:h-auto md:min-w-[18rem]">
+    <a className="flex space-x-3 cursor-pointer border-off-white border-[1.5px] rounded-xl p-3" href={href}>
+        <div className="relative h-[18%] w-[18%] max-w-[64px] flex-none">
+            <div className="pb-[100%] w-full" />
             <div className="absolute bg-transparent dark:bg-off-black !bg-opacity-25 w-full h-full z-10" />
-            <Image src={image} alt={imageAlt} className="rounded-t-md md:rounded-t-none md:!rounded-r-lg" layout="fill" objectFit="cover" priority={false} />
+            <Image src={image} alt={imageAlt} className="rounded-lg" layout="fill" objectFit="cover" priority={false} />
         </div>
-        <div className="border-[#d0d7de] dark:border-[#333333] bg-off-white bg-opacity-25 dark:bg-[#1A1A1A] 
-        border-x border-b md:border-y md:border-r-0 
-        px-4 pt-3 pb-4 md:pt-0 md:pb-4 md:pl-7
-        rounded-b-lg md:rounded-l-lg md:rounded-r-none
-        md:flex justify-center flex-col space-y-1">
-            <h3 className="mono-bold text-main-light dark:text-main-dark text-lg md:text-xl">{ title }</h3>
-            <p className="mono-regular text-sub-light dark:text-sub-dark text-xs sm:text-sm">{ text }</p>
+        <div>
+            <h2 className="font-mono font-semibold leading-snug text-main-light dark:text-main-dark">{ title }</h2>
+            <p className="font-mono text-xs leading-snug text-sub-light dark:text-sub-dark">{ text }</p>
         </div>
     </a>
     )
