839af9a1c764333a25347b2be3fb470a02c263e8
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-21 02:27:13 -0400

    Profile dark mode

--- frames/Profile.tsx
+++ frames/Profile.tsx
@@ -10,13 +10,13 @@ const Profile = () => {
     <div>
         <div className="flex justify-between mb-6">
             <div className="relative min-w-[80px] w-[30%] h-[30%] -mt-[18%] rounded-full overflow-hidden">
-                <div className="pb-[100%] w-full bg-white" />
+                <div className="pb-[100%] w-full bg-white dark:bg-dark" />
                 {/* text-[0px] because for some strange reason inline block adds spaces... */}
                 <div className="top-[4px] left-[4px] absolute w-[calc(100%-8px)] h-[calc(100%-8px)] text-[0px] rounded-full overflow-hidden">
                     <Image src={profile} />
                 </div>
             </div>
-            <div className="flex w-[140px] justify-between text-sub-light text-2xl self-end pr-1 pb-1 pt-4"> 
+            <div className="flex w-[140px] justify-between text-sub-light dark:text-sub-dark text-2xl self-end pr-1 pb-1 pt-4"> 
                 <a className="flex">
                     <FontAwesomeIcon icon={faGithub} />
                 </a>
@@ -28,8 +28,12 @@ const Profile = () => {
                 </a>
             </div>
         </div>
-        <h1 className="font-inter font-bold text-main-light text-xl mb-1">Hello! I'm Kevin Liao.</h1>
-        <p className="font-inter font-medium text-sub-light text-sm leading-snug">I'm a full stack developer, designer, and software engineer from New York.</p>
+        <h1 className="font-inter font-bold text-main-light dark:text-main-dark text-xl mb-1">
+            Hello! I'm Kevin Liao.
+        </h1>
+        <p className="font-inter font-medium text-sub-light dark:text-main-dark text-sm leading-snug">
+            I'm a full stack developer, designer, and software engineer from New York.
+        </p>
     </div>
     )
 }
