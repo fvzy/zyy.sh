a01c6184602c03aa5730e714c8a1d20401ab4f9c
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-21 02:32:03 -0400

    Contact dark mode

--- frames/Contact.tsx
+++ frames/Contact.tsx
@@ -4,15 +4,15 @@ import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 const Contact = () => {
     return (
         <div className="flex items-center flex-col pt-7">
-            <h1 className="font-mono font-bold text-sub-light text-lg mb-1">
+            <h1 className="font-mono font-bold text-sub-light dark:text-sub-dark text-lg mb-1">
                 Contact Me
             </h1>
-            <p className="font-inter text-sm leading-snug text-center max-w-[358px]">
+            <p className="font-inter text-sub-light dark:text-sub-dark text-sm leading-snug text-center max-w-[358px]">
                 I'm always looking for opportunities to work on new and exciting
                 projects. Click the button below to send me an email, and let's
                 start working!
             </p>
-            <a href="mailto:hello@liao.gg" className="mt-4 inline-flex text-sub-light items-center space-x-3 border-off-white border-[1.5px] py-3 px-6 rounded-lg text-sm">
+            <a href="mailto:hello@liao.gg" className="mt-4 inline-flex text-sub-light items-center space-x-3 border-off-white dark:border-off-black border-[1.5px] py-3 px-6 rounded-lg text-sm">
                 <FontAwesomeIcon icon={faEnvelope} className="pb-[2px]" /><p className="font-mono font-semibold">Email Me</p>
             </a>
         </div>
