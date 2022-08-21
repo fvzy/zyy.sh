98f654a620043493a89eda36a0f8ac8f3112ac04
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-21 14:06:53 -0400

    Profile and banner max size changes

--- frames/Banner.tsx
+++ frames/Banner.tsx
@@ -5,10 +5,10 @@ const Banner = () => {
     return (
     <>
     <div className="relative overflow-hidden">
-        <div className="absolute top-0 left-0 w-full h-full">
+        <div className="absolute top-0 left-0 w-full h-full max-h-[200px]">
             <Image src={banner} layout="fill" objectFit="cover" />
         </div>
-        <div className="pb-[35%] w-full dark:bg-off-black opacity-40" />
+        <div className="pb-[min(35%,200px)] w-full dark:bg-off-black opacity-40" />
     </div>
     </>)
 }
