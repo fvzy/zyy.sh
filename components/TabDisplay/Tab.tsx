dd651eb61a8f5f4d7bd75b5843c3e312202f5375
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-21 13:41:04 -0400

    Border 1px instead of 1.5px 1.5 px doesn't make a difference vs 1 save for some slight spacing changes

--- components/TabDisplay/Tab.tsx
+++ components/TabDisplay/Tab.tsx
@@ -12,7 +12,7 @@ const Tab =
     return(
     <div className="inline-block flex-shrink-0 h-10 w-28 cursor-pointer transition-all" onClick={setSelfActive}>
         <div className={"rounded-md h-full w-full pt-[2px] flex items-center justify-center font-mono text-sm " + 
-        (active ? `border-off-white dark:border-off-black border-[1.5px] text-quiet-light dark:text-quiet-dark font-semibold`
+        (active ? `border-off-white dark:border-off-black border text-quiet-light dark:text-quiet-dark font-semibold`
         : `text-quiet-light dark:text-quiet-dark font-medium` )}>
             {children}
         </div>
