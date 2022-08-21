19001ced882c5f3c4f1826a36d3e1373c87cd602
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-21 14:07:01 -0400

    ProjectCard md styling

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,7 +4,7 @@ const ProjectCard =
 ({ image, imageAlt, title, text, href }: 
 { image: string | StaticImageData | { default: StaticImageData }, imageAlt: string, title: string, text: string, href: string }) => {
     return(
-    <a className="flex space-x-3 cursor-pointer border-off-white dark:border-off-black border rounded-xl p-3" href={href}>
+    <a className="flex space-x-3 md:space-x-5 cursor-pointer border-off-white dark:border-off-black border rounded-xl p-3 md:py-3.5 md:px-4" href={href}>
         <div className="relative h-[18%] w-[18%] max-w-[64px] flex-none">
             <Image src={image} alt={imageAlt} className="rounded-lg" layout="fill" objectFit="cover" priority={false} />
             <div className="pb-[100%] w-full dark:bg-off-black opacity-30" />
