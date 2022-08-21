5f344ef6cdffabda59c58bcab6e71cfed7e6609c
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-21 14:36:17 -0400

    Slide md styling + removed margin on last div

--- components/TabDisplay/Slide.tsx
+++ components/TabDisplay/Slide.tsx
@@ -14,18 +14,18 @@ const Slide = ({ skillGroups, active }: SlideProps) => {
     <div className={"absolute overflow-scroll flex justify-between -mx-7 px-7 space-x-6 no-scrollbar transition-opacity left-0 right-0 " + (active ? "z-10 opacity-100 " : "opacity-0 ")}>
         {skillGroups.map(({title, skills}, i) => (
         <div className="min-w-[200px] flex-shrink-0 inline-block" key={i}>
-            <h2 className="font-mono font-semibold text-sub-light dark:text-sub-dark pb-2">{ title }</h2>
-            <ul className="font-mono font-medium text-sub-light dark:text-sub-dark pl-2.5 space-y-1">
+            <h2 className="font-mono font-semibold text-sub-light dark:text-sub-dark pb-2 md:pb-3">{ title }</h2>
+            <ul className="font-mono font-medium text-sub-light dark:text-sub-dark pl-2.5 md:pl-3.5 space-y-1 md:space-y-2">
                 {skills.map(([icon, text], j) => 
-                    <li key={j} className="flex items-center space-x-3 text-sm">
-                        <FontAwesomeIcon icon={icon} fixedWidth={true} className="text-base pb-[2px]" />
-                        <p className="inline">{text}</p>
-                    </li>)
+                <li key={j} className="flex items-center space-x-3 md:space-x-4 text-sm">
+                    <FontAwesomeIcon icon={icon} fixedWidth={true} className="text-base pb-[2px]" />
+                    <p className="inline">{text}</p>
+                </li>)
                 }
             </ul>
         </div>
         ))}
-        <div></div>
+        <div className="!m-0"></div>
     </div>
     )
 }
