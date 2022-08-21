16370753a1476f9fc42a6ef6e5c66f5b90f5e4c4
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-21 02:26:30 -0400

    Banner dark mode off black overlay

--- frames/Banner.tsx
+++ frames/Banner.tsx
@@ -5,10 +5,10 @@ const Banner = () => {
     return (
     <>
     <div className="relative overflow-hidden">
-        <div className="pb-[35%] w-full" />
         <div className="absolute top-0 left-0 w-full h-full">
             <Image src={banner} layout="fill" objectFit="cover" />
         </div>
+        <div className="pb-[35%] w-full dark:bg-off-black opacity-40" />
     </div>
     </>)
 }
