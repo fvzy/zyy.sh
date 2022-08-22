6bd360769ae6d5c623c796fe72b65432a1719010
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-22 00:56:43 -0400

    ProjectCard transition and rounded fixes

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,10 +4,10 @@ const ProjectCard =
 ({ image, imageAlt, title, text, href }: 
 { image: string | StaticImageData | { default: StaticImageData }, imageAlt: string, title: string, text: string, href: string }) => {
     return(
-    <a className="flex space-x-3 md:space-x-5 cursor-pointer border-off-white dark:border-off-black border rounded-xl p-3 md:py-3.5 md:px-4" href={href}>
+    <a className="flex space-x-3 md:space-x-5 cursor-pointer border-off-white dark:border-off-black transition-all duration-300 border p-3 md:py-3.5 md:px-4" href={href}>
         <div className="relative h-[18%] w-[18%] max-w-[70px] flex-none">
             <Image src={image} alt={imageAlt} className="rounded-lg" layout="fill" objectFit="cover" priority={false} />
-            <div className="pb-[100%] w-full dark:bg-off-black opacity-30" />
+            <div className="pb-[100%] w-full dark:bg-off-black opacity-30 rounded-lg" />
         </div>
         <div>
             <h2 className="font-mono font-semibold md:text-lg leading-snug text-main-light dark:text-main-dark">{ title }</h2>
