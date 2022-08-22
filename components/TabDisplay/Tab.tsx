a766601ed830308f50bd4c6b8678a0fdbe5ea56c
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-22 00:56:54 -0400

    Tab transition

--- components/TabDisplay/Tab.tsx
+++ components/TabDisplay/Tab.tsx
@@ -10,10 +10,10 @@ const Tab =
     }
 
     return(
-    <div className="inline-block flex-shrink-0 h-10 w-28 cursor-pointer transition-all" onClick={setSelfActive}>
-        <div className={"rounded-md h-full w-full pt-[2px] flex items-center justify-center font-mono text-sm " + 
-        (active ? `border-off-white dark:border-off-black border text-quiet-light dark:text-quiet-dark font-semibold`
-        : `text-quiet-light dark:text-quiet-dark font-medium` )}>
+    <div className="inline-block flex-shrink-0 h-10 w-28 cursor-pointer" onClick={setSelfActive}>
+        <div className={"rounded-md h-full w-full pt-[2px] flex items-center justify-center font-mono text-sm transition-all duration-300 border-off-white dark:border-off-black border " + 
+        (active ? `text-quiet-light dark:text-quiet-dark font-semibold !border-opacity-100`
+        : `text-quiet-light dark:text-quiet-dark font-medium !border-opacity-0` )}>
             {children}
         </div>
     </div>
