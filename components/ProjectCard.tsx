eb3e179be758c8b7af84c4adbbf6b06548ca8cd5
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-08-22 01:00:48 -0400

    Project card rounded fix + padding changes

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,8 +4,8 @@ const ProjectCard =
 ({ image, imageAlt, title, text, href }: 
 { image: string | StaticImageData | { default: StaticImageData }, imageAlt: string, title: string, text: string, href: string }) => {
     return(
-    <a className="flex space-x-3 md:space-x-5 cursor-pointer border-off-white dark:border-off-black transition-all duration-300 border p-3 md:py-3.5 md:px-4" href={href}>
-        <div className="relative h-[18%] w-[18%] max-w-[70px] flex-none">
+    <a className="flex space-x-3 md:space-x-5 cursor-pointer border-off-white dark:border-off-black transition-all duration-300 border p-3 md:py-4 md:px-[18px] rounded-xl" href={href}>
+        <div className="relative h-[18%] w-[18%] max-w-[64px] flex-none">
             <Image src={image} alt={imageAlt} className="rounded-lg" layout="fill" objectFit="cover" priority={false} />
             <div className="pb-[100%] w-full dark:bg-off-black opacity-30 rounded-lg" />
         </div>
