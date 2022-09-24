6a57a69d56bba63d8d84ce94f708cf9ac67f8f0f
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-09-24 13:42:09 -0400

    Misc changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,8 @@ import { faStar } from "@fortawesome/free-regular-svg-icons";
 const Footer = () => {
     return(
     <footer className="space-y-1 flex flex-col items-center text-xs mb-8 md:mb-12 lg:mb-14 font-medium">
-        <p className="font-inter text-sub-light dark:text-sub-dark">Made with magic by kevinMEH.</p>
         <a className="flex space-x-1 font-mono text-quiet-light dark:text-quiet-dark items-center" href="https://github.com/kevinMEH/portfolio">
-            <FontAwesomeIcon icon={faStar} className="pb-[0.5px]" />
+            <FontAwesomeIcon icon={faStar} className="pb-[1px]" />
             <p>kevinMEH/portfolio</p>
         </a>
     </footer>
