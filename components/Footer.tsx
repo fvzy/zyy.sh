49301358563f78d1adbdb22e062a7bc3ef113ade
Ditzzy <mukhamadnurshodiqarridho_1701617092@mhs.unj.ac.id>
2023-02-07 01:07:07 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { faStar } from "@fortawesome/free-regular-svg-icons";
 const Footer = () => {
     return(
     <footer className="space-y-1 flex flex-col items-center text-xs mb-8 md:mb-12 lg:mb-14 font-medium">
-        <a className="flex space-x-1 font-mono text-quiet-light dark:text-quiet-dark items-center" href="https://github.com/kevinMEH/portfolio">
+        <a className="flex space-x-1 font-mono text-quiet-light dark:text-quiet-dark items-center" href="https://Instagram.com/ditzzxsz_">
             <FontAwesomeIcon icon={faStar} className="pb-[1px]" />
             <p>Ditzzsenpai ❤️</p>
         </a>
